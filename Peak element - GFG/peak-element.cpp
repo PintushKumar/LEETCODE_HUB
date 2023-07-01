1185539412f4696ec25f62fb33ad8d91546c1abb
Pintush  Kumar <pintushmitcse@gmail.com>
2023-07-01 22:40:26 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.cpp
+++ Peak element - GFG/peak-element.cpp
@@ -16,8 +16,8 @@ class Solution
     {
        // Your code here
        if(n==1) return 0;
-       if(arr[0]>arr[1]) return 0;
-       if(arr[n-1]>=arr[n-2]) return n-1;
+       if(arr[0]>=arr[1]) return 0;
+       if(arr[n-1]>arr[n-2]) return n-1;
        int low =0 ,high = n-2;
        while(low<= high){
            int mid =(low+high)/2;
