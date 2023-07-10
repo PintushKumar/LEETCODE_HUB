82109581f2f12b22a8c9d1a637c29e14d9c28822
Pintush  Kumar <pintushmitcse@gmail.com>
2023-07-10 07:57:49 +0530

    Time: 1 ms (99.75%), Space: 44 MB (37.39%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,22 +1,39 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
-       int n = nums.length;
-       int [] lsum = new int[n];
-       int rsum[] = new int[n];
-       int res[] = new int[n];
-        int pre =0;
-        for(int i=0;i<n;i++){
-            lsum[i]=pre;
-            pre+=nums[i];
+        
+//*******************  My bruteForce Solution *************************
+       // int n = nums.length;
+       // int [] lsum = new int[n];
+       // int rsum[] = new int[n];
+       // int res[] = new int[n];
+       //  int pre =0;
+       //  for(int i=0;i<n;i++){
+       //      lsum[i]=pre;
+       //      pre+=nums[i];
+       //  }
+       //  pre=0;
+       //  for(int i=n-1;i>=0;i--){
+       //      rsum[i]=pre;
+       //      pre+=nums[i];
+       //  }
+       //  for(int i=0;i<n;i++){
+       //      res[i]=Math.abs(lsum[i]-rsum[i]);
+       //  }
+       //  return res;
+        
+// **********************************************************************
+        
+        int totalSum =0 , rightSum = 0 , leftSum =0 ;
+        for(int ele: nums){
+            totalSum+=ele;
         }
-        pre=0;
-        for(int i=n-1;i>=0;i--){
-            rsum[i]=pre;
-            pre+=nums[i];
+        for(int i=0;i<nums.length;i++){
+            int val = nums[i];
+            rightSum = totalSum - val;
+            totalSum -= val;
+            nums[i] = Math.abs(leftSum - rightSum);
+            leftSum+=val;
         }
-        for(int i=0;i<n;i++){
-            res[i]=Math.abs(lsum[i]-rsum[i]);
-        }
-        return res;
+        return nums;
     }
 }
