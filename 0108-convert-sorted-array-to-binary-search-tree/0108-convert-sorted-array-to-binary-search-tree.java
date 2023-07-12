5c894d2e30c64e2cf0584c176c14839ef1e0dff6
Pintush  Kumar <pintushmitcse@gmail.com>
2023-07-12 09:57:41 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (51.97%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -14,18 +14,34 @@
  * }
  */
 class Solution {
-    public TreeNode constructTree(int []nums , int l , int r){
-        if(l>r){
+    public TreeNode constructTree(int[] nums, int l, int r) {
+        // Check if the left index is greater than the right index
+        if (l > r) {
+            // If so, return null since there are no elements to construct the tree
             return null;
         }
-        int midIndx = (l+r)/2;
+        
+        // Calculate the middle index
+        int midIndx = (l + r) / 2;
+        
+        // Create a new TreeNode with the value at the middle index
         TreeNode root = new TreeNode(nums[midIndx]);
-        root.left = constructTree(nums , l , midIndx-1);
-        root.right = constructTree(nums ,midIndx+1 , r);
+        
+        // Recursively construct the left subtree using the left portion of the array
+        root.left = constructTree(nums, l, midIndx - 1);
+        
+        // Recursively construct the right subtree using the right portion of the array
+        root.right = constructTree(nums, midIndx + 1, r);
+        
+        // Return the root of the constructed binary search tree
         return root;
     }
+    
     public TreeNode sortedArrayToBST(int[] nums) {
-        TreeNode root = constructTree(nums , 0 , nums.length-1);
+        // Construct the binary search tree using the entire nums array
+        TreeNode root = constructTree(nums, 0, nums.length - 1);
+        
+        // Return the root of the constructed binary search tree
         return root;
     }
 }
