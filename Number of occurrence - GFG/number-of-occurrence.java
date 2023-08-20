f8f592f2dfef785503aabb064cf57389df0b8f72
Pintush  Kumar <pintushmitcse@gmail.com>
2023-08-20 13:51:26 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.java
+++ Number of occurrence - GFG/number-of-occurrence.java
@@ -36,50 +36,30 @@ public class Main {
 
 
 class Solution {
-    public int firstIndex(int []nums , int n , int target){
-        int low =0 , high = n-1 , pans =-1;
+    int count(int[] arr, int n, int x) {
+        // code here
+        int stind =-1;
+        int edind =-1;
+        int low = 0 , high = n-1;
         while(low<=high){
-            int mid =(low+high)/2;
-            if(nums[mid]== target){
-                pans = mid;
-                high = mid-1;  
-            }else if(nums[mid]<target){
-                low = mid+1;
+            int mid = (low+high)/2;
+            if(arr[mid]== x){
+                stind = mid;
+                edind = mid;
+                while(stind>=0 &&arr[stind] == x){
+                    stind--;
+                }
+                while(edind<=n-1 && arr[edind]== x){
+                    edind++;
+                }
+                return (edind-stind)-1;
+            }
+            if(arr[mid]<x){
+                low=mid+1;
             }else{
                 high = mid-1;
             }
         }
-        return pans;
-    }
-
-    public int lastIndex(int []nums , int n , int target){
-        int low =0 , high = n-1 , pans =-1;
-        while(low<=high){
-            int mid =(low+high)/2;
-            if(nums[mid]== target){
-                pans = mid;
-                low = mid+1;
-            }else if(nums[mid]<target){
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
-        }
-        return pans;
-    }
-    int count(int[] nums, int n, int target) {
-        // int count = 0;
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]==x){
-        //         count++;
-        //     }
-        // }
-        // return count;
-        int firstInd = firstIndex(nums , n , target);
-        if(firstInd==-1){
-            return 0;
-        }
-        int lastInd = lastIndex(nums , n , target);
-        return lastInd-firstInd+1;
+        return 0;
     }
 }
