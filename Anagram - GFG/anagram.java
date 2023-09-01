c62081e0afc0b4af428f46752b76e90478b83a92
Pintush  Kumar <pintushmitcse@gmail.com>
2023-09-01 12:59:52 +0530

    Added solution - LeetHub

--- Anagram - GFG/anagram.java
+++ Anagram - GFG/anagram.java
@@ -37,41 +37,26 @@ class GFG {
 class Solution
 {    
     //Function is to check whether two strings are anagram of each other or not.
-    public static boolean isAnagram(String c1,String c2)
+    public static boolean isAnagram(String a,String b)
     {
         
         // Your code here
-        // if(a.length() != b.length()){
-        //     return false;
-        // }
-        // int freqArr[] = new int [26];
-        // for(int i=0;i<a.length();i++){
-        //     freqArr[a.charAt(i)-97]++;
-        // }
-        // for(int i=0;i<b.length();i++){
-        //     freqArr[a.charAt(i)-97]--;
-        // }
-        // for(int i=0;i<26;i++){
-        //     if(freqArr[i] != 0){
-        //         return false;
-        //     }
-        // }
-        // return true;
-        if(c1.length() != c2.length()){
-		   return false;
-	   }
-	   int count [] = new int[26];
-	   for(int i=0;i<c1.length();i++){
-		   count[c1.charAt(i)-97]++;
-	   }
-	   for(int i=0;i<c2.length();i++){
-		   count[c2.charAt(i)-97]--;
-	   }
-	   for(int i=0;i<26;i++){
-		   if(count[i] != 0){
-			   return false;
-		   }
-	   }
-	   return true;
+        if(a.length() != b.length()){
+            return false;
+        }
+        int freqArr[] = new int [26];
+        for(int i=0;i<a.length();i++){
+            freqArr[a.charAt(i)-97]++;
+        }
+        for(int i=0;i<b.length();i++){
+            freqArr[b.charAt(i)-97]--;
+        }
+        for(int i=0;i<26;i++){
+            if(freqArr[i] != 0){
+                return false;
+            }
+        }
+        return true;
+
     }
 }
