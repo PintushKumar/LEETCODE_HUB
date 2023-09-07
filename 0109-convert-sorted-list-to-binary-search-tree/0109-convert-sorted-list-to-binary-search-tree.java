3ec92832657c1db79e4bd39c38288722eb984d19
Pintush  Kumar <pintushmitcse@gmail.com>
2023-09-07 16:06:48 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (99.43%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -25,27 +25,39 @@
  */
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
-        if(head == null){
+        // Base case: If the input linked list is empty, return null.
+        if (head == null) {
             return null;
         }
-        if(head.next == null){
+        
+        // Base case: If there's only one element in the linked list, create a TreeNode with that value and return it.
+        if (head.next == null) {
             return new TreeNode(head.val);
         }
         
+        // Initialize two pointers: slow and fast for finding the middle of the linked list.
         ListNode slow = head;
         ListNode fast = head;
-        ListNode prev_slow = null;
-        
-        while(fast != null && fast.next != null){
-            prev_slow = slow;
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode prev_slow = null; // Pointer to the node just before 'slow'.
+
+        // Traverse the linked list to find the middle using the slow and fast pointers.
+        while (fast != null && fast.next != null) {
+            prev_slow = slow; // Update prev_slow to the current 'slow'.
+            slow = slow.next; // Move 'slow' one step at a time.
+            fast = fast.next.next; // Move 'fast' two steps at a time.
         }
-        
+
+        // Create a TreeNode with the value of the middle node found.
         TreeNode root = new TreeNode(slow.val);
+
+        // Break the linked list into two halves by updating 'prev_slow' and setting 'slow.next' to null.
         prev_slow.next = null;
-        root.left = sortedListToBST(head);
-        root.right= sortedListToBST(slow.next);
+
+        // Recursively build the left and right subtrees using the two halves of the linked list.
+        root.left = sortedListToBST(head); // Left subtree: Nodes before the middle.
+        root.right = sortedListToBST(slow.next); // Right subtree: Nodes after the middle.
+
+        // Return the root of the resulting binary search tree.
         return root;
     }
 }
