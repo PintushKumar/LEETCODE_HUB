15fa6b108109d35f2d44e953031b0deea3ac52be
Pintush  Kumar <pintushmitcse@gmail.com>
2023-09-10 10:51:17 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -31,36 +31,85 @@ class DriverClass {
 
 /*Complete the function below*/
 
+
+// ********************** Detecting Cycle Using DFS ******************************
+
+// class Solution {
+//     public boolean isCyclicDFS(ArrayList<ArrayList<Integer>> adj , int node , boolean visited[] ,boolean dfsVisited[]){
+//         visited[node] = true;
+//         dfsVisited[node] = true;
+        
+//         for(int neighbor : adj.get(node)){
+//             if(!visited[neighbor]){
+//                 if(isCyclicDFS(adj , neighbor , visited , dfsVisited)){
+//                     return true;
+//                 }
+//             }else if(dfsVisited[neighbor]){
+//                 return true;
+//             }
+//         }
+//         dfsVisited[node]= false;
+//         return false;
+//     }
+//     // Function to detect cycle in a directed graph.
+//     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+//         // code here
+//         boolean visited[] = new boolean[V];
+//         boolean dfsVisited [] = new boolean[V];
+        
+//         for(int i=0;i<V;i++){
+//             if(!visited[i]){
+//                 if(isCyclicDFS(adj, i , visited , dfsVisited )){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+// ************************ Detecting Cycle using BFS **************************
+
 class Solution {
-    public boolean isCyclicDFS(ArrayList<ArrayList<Integer>> adj , int node , boolean visited[] ,boolean dfsVisited[]){
-        visited[node] = true;
-        dfsVisited[node] = true;
-        
-        for(int neighbor : adj.get(node)){
-            if(!visited[neighbor]){
-                if(isCyclicDFS(adj , neighbor , visited , dfsVisited)){
-                    return true;
-                }
-            }else if(dfsVisited[neighbor]){
-                return true;
-            }
-        }
-        dfsVisited[node]= false;
-        return false;
-    }
     // Function to detect cycle in a directed graph.
-    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+    public boolean isCyclic(int N, ArrayList<ArrayList<Integer>> adj) {
         // code here
-        boolean visited[] = new boolean[V];
-        boolean dfsVisited [] = new boolean[V];
+        Queue<Integer> q = new ArrayDeque<>();
+        int inDegree[] = new int [N];
         
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                if(isCyclicDFS(adj, i , visited , dfsVisited )){
-                    return true;
+        // calculate in Degree 
+        
+        for(int u=0;u<N;u++){
+            for(int v:adj.get(u)){
+                inDegree[v]++;
+            }
+        }
+        
+        for(int i=0;i<N;i++){
+            if(inDegree[i]==0){
+                q.add(i);
+            }
+        }
+        
+        int countNode =q.size();
+        while(q.size()>0){
+            int u = q.remove();
+            for(int v:adj.get(u)){
+                inDegree[v]--;
+                if(inDegree[v]==0){
+                    q.add(v);
+                    countNode +=1;
                 }
             }
         }
-        return false;
+        
+        if(countNode == N){
+            return false;
+        }
+        
+        return true;
     }
 }
+
+
