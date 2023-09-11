c4c4bf59d800ccb59c88e70989e926fe84f5e5d2
Pintush  Kumar <pintushmitcse@gmail.com>
2023-09-11 15:30:23 +0530

    Added solution - LeetHub

--- Prerequisite Tasks - GFG/prerequisite-tasks.java
+++ Prerequisite Tasks - GFG/prerequisite-tasks.java
@@ -40,22 +40,51 @@ class GFG {
 
 //User function Template for Java
 
+// class Solution {
+//     public boolean isCyclicDFS(ArrayList<ArrayList<Integer>>adj , int node , boolean [] visited , boolean []dfsVisited){
+//         visited[node]=true;
+//         dfsVisited[node]=true;
+//         for(int v:adj.get(node)){
+//             if(!visited[v]){
+//                 if(isCyclicDFS(adj , v , visited , dfsVisited)){
+//                     return true;
+//                 }
+//             }else if(dfsVisited[v]==true){
+//                 return true;
+//             }
+//         }
+//         dfsVisited[node]=false;
+//         return false;
+//     }
+//     public boolean isPossible(int N,int P, int[][] prerequisites)
+//     {
+//         // Your Code goes here
+//         ArrayList<ArrayList<Integer>>adj = new ArrayList<>();
+//         for(int i=0;i<N;i++){
+//             adj.add(new ArrayList<Integer>());
+//         }
+//         for(int [] course:prerequisites){
+//             int a = course[0];
+//             int b = course[1];
+//             adj.get(b).add(a);
+//         }
+        
+//         boolean visited[] = new boolean[N];
+//         boolean dfsVisited[]= new boolean[N];
+        
+//         for(int i=0;i<N;i++){
+//             if(!visited[i]){
+//                 if(isCyclicDFS(adj , i , visited , dfsVisited)){
+//                     return false;
+//                 }
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
+
 class Solution {
-    public boolean isCyclicDFS(ArrayList<ArrayList<Integer>>adj , int node , boolean [] visited , boolean []dfsVisited){
-        visited[node]=true;
-        dfsVisited[node]=true;
-        for(int v:adj.get(node)){
-            if(!visited[v]){
-                if(isCyclicDFS(adj , v , visited , dfsVisited)){
-                    return true;
-                }
-            }else if(dfsVisited[v]==true){
-                return true;
-            }
-        }
-        dfsVisited[node]=false;
-        return false;
-    }
     public boolean isPossible(int N,int P, int[][] prerequisites)
     {
         // Your Code goes here
@@ -69,18 +98,40 @@ class Solution {
             adj.get(b).add(a);
         }
         
-        boolean visited[] = new boolean[N];
-        boolean dfsVisited[]= new boolean[N];
+        int inDegree[] = new int[N];
+        for(int u=0;u<N;u++){
+            for(int v:adj.get(u)){
+                inDegree[v]++;
+            }
+        }
         
+        Queue<Integer> q= new ArrayDeque<>();
         for(int i=0;i<N;i++){
-            if(!visited[i]){
-                if(isCyclicDFS(adj , i , visited , dfsVisited)){
-                    return false;
+            if(inDegree[i]==0){
+                q.add(i);
+            }
+        }
+        
+        if(q.size()==0){
+            return false;
+        }
+        int count = q.size();
+        while(q.size()>0){
+            int u = q.remove();
+            for(int v:adj.get(u)){
+                inDegree[v]--;
+                if(inDegree[v]==0){
+                    q.add(v);
+                    count+=1;
                 }
             }
         }
         
-        return true;
+        if(count==N){
+            return true;
+        }
+      
+        
+        return false;
     }
-    
 }
