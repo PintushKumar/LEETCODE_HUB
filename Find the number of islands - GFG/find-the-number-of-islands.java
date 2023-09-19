28a0d960f23b53674d1426fcc0f2c5195444be47
Pintush  Kumar <pintushmitcse@gmail.com>
2023-09-19 20:41:32 +0530

    Added solution - LeetHub

---
+++ Find the number of islands - GFG/find-the-number-of-islands.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            char[][] grid = new char[n][m];
+            for (int i = 0; i < n; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                for (int j = 0; j < m; j++) {
+                    grid[i][j] = S[j].charAt(0);
+                }
+            }
+            Solution obj = new Solution();
+            int ans = obj.numIslands(grid);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public void dfs(char [][] grid , int i , int j , int n  , int m ){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]=='0'){
+            return;
+        }
+        
+        grid[i][j] = '0';  // mark
+        
+        dfs(grid , i-1 , j , n, m);
+        dfs(grid , i+1 , j , n, m);
+        dfs(grid , i , j-1 , n, m);
+        dfs(grid , i , j+1 , n, m);
+        dfs(grid , i-1 , j-1 , n, m);
+        dfs(grid , i+1 , j+1 , n, m);
+        dfs(grid , i-1 , j+1 , n, m);
+        dfs(grid , i+1 , j-1 , n, m);
+    }
+    // Function to find the number of islands.
+    public int numIslands(char[][] grid) {
+        // Code here
+        int n = grid.length;
+        int m = grid[0].length;
+        int isLands = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    isLands++;
+                    dfs(grid , i , j , n , m);
+                }
+            }
+        }
+        return isLands;
+    }
+}
