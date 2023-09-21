5c78505cf490a972697bde643d81e84352e2f686
Pintush  Kumar <pintushmitcse@gmail.com>
2023-09-21 18:01:45 +0530

    Added solution - LeetHub

--- Shortest Source to Destination Path - GFG/shortest-source-to-destination-path.java
+++ Shortest Source to Destination Path - GFG/shortest-source-to-destination-path.java
@@ -31,12 +31,12 @@ class GFG {
 
 // User function Template for Java
 
-class Pair{
-    int row ;
-    int col ;
-    int dist ;
+class Pair {
+    int row;   // Row coordinate of the cell
+    int col;   // Column coordinate of the cell
+    int dist;  // Distance from the start cell to this cell
     
-    Pair(int row , int col , int dist){
+    Pair(int row, int col, int dist) {
         this.row = row;
         this.col = col;
         this.dist = dist;
@@ -45,25 +45,34 @@ class Pair{
 
 class Solution {
     int shortestDistance(int N, int M, int A[][], int X, int Y) {
-        // code here
+        // Create a queue to perform breadth-first search (BFS)
         Queue<Pair> q = new ArrayDeque<>();
-        q.add(new Pair(0 , 0 , 0));
-        while(q.size()>0){
-            Pair temp = q.remove();
-            int row = temp.row;
-            int col = temp.col;
-            int dist = temp.dist;
-            if(row>=0 && row<N && col>=0 && col <M && A[row][col] !=0){
-                if(row == X && col == Y){
+        q.add(new Pair(0, 0, 0));  // Add the starting cell (0, 0) with distance 0
+        
+        while (q.size() > 0) {
+            Pair temp = q.remove();  // Dequeue the cell from the queue
+            int row = temp.row;     // Get the row coordinate of the current cell
+            int col = temp.col;     // Get the column coordinate of the current cell
+            int dist = temp.dist;   // Get the distance to the current cell
+            
+            // Check if the current cell is within bounds and not blocked (0)
+            if (row >= 0 && row < N && col >= 0 && col < M && A[row][col] != 0) {
+                if (row == X && col == Y) {
+                    // If we reached the destination cell (X, Y), return the distance
                     return dist;
                 }
-                A[row][col] = 0;
-                q.add(new Pair(row+1 , col , dist+1));
-                q.add(new Pair(row-1 , col , dist+1));
-                q.add(new Pair(row , col+1 , dist+1));
-                q.add(new Pair(row , col-1 , dist+1));
+                
+                A[row][col] = 0;  // Mark the current cell as visited (set to 0)
+                
+                // Add adjacent cells to the queue with an increased distance
+                q.add(new Pair(row + 1, col, dist + 1));  // Down
+                q.add(new Pair(row - 1, col, dist + 1));  // Up
+                q.add(new Pair(row, col + 1, dist + 1));  // Right
+                q.add(new Pair(row, col - 1, dist + 1));  // Left
             }
         }
+        
+        // If no path is found from the start to the destination, return -1
         return -1;
     }
-};
+}
