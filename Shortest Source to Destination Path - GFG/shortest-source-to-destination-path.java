348ea778dd655435840829bb7e51d81f5507b25e
Pintush  Kumar <pintushmitcse@gmail.com>
2023-09-21 18:00:12 +0530

    Added solution - LeetHub

--- Shortest Source to Destination Path - GFG/shortest-source-to-destination-path.java
+++ Shortest Source to Destination Path - GFG/shortest-source-to-destination-path.java
@@ -31,62 +31,39 @@ class GFG {
 
 // User function Template for Java
 
-class Pair {
-    int first;  // Distance from source
-    int second; // Row coordinate
-    int third;  // Column coordinate
-
-    Pair(int first, int second, int third) {
-        this.first = first;
-        this.second = second;
-        this.third = third;
+class Pair{
+    int row ;
+    int col ;
+    int dist ;
+    
+    Pair(int row , int col , int dist){
+        this.row = row;
+        this.col = col;
+        this.dist = dist;
     }
 }
 
 class Solution {
     int shortestDistance(int N, int M, int A[][], int X, int Y) {
-        // If the source cell is blocked, return -1
-        if (A[0][0] == 0) return -1;
-
-        // Create a queue for storing cells with their distances from the source
-        // in the form {dist, {cell coordinates pair}}.
+        // code here
         Queue<Pair> q = new ArrayDeque<>();
-        q.add(new Pair(0, 0, 0));
-
-        // Arrays for representing possible movements: down, up, right, left
-        int[] dr = {1, -1, 0, 0};
-        int[] dc = {0, 0, 1, -1};
-        //{1, 0}: Move down one row.
-        // {-1, 0}: Move up one row.
-        // {0, 1}: Move right one column.
-        // {0, -1}: Move left one column.
-        while (!q.isEmpty()) {
+        q.add(new Pair(0 , 0 , 0));
+        while(q.size()>0){
             Pair temp = q.remove();
-            int dis = temp.first;  // Distance of the current cell from the source
-            int r = temp.second;   // Row coordinate of the current cell
-            int c = temp.third;    // Column coordinate of the current cell
-
-            // If the current cell is the destination, return the distance
-            if (r == X && c == Y) return dis;
-
-            // Explore all 4 directions from the current cell
-            for (int i = 0; i < 4; i++) {
-                int newr = r + dr[i];     // New row coordinate
-                int newc = c + dc[i];     // New column coordinate
-
-                // Check if the new coordinates are within bounds and the cell is not blocked
-                if (newr >= 0 && newr < N && newc >= 0 && newc < M && A[newr][newc] == 1) {
-                    A[newr][newc] = 0;  // Mark the cell as visited
-                    // If the new cell is the destination, return the updated distance
-                    if (newr == X && newc == Y) return dis + 1;
-                    // Add the new cell to the queue with updated distance
-                    q.add(new Pair(1 + dis, newr, newc));
+            int row = temp.row;
+            int col = temp.col;
+            int dist = temp.dist;
+            if(row>=0 && row<N && col>=0 && col <M && A[row][col] !=0){
+                if(row == X && col == Y){
+                    return dist;
                 }
+                A[row][col] = 0;
+                q.add(new Pair(row+1 , col , dist+1));
+                q.add(new Pair(row-1 , col , dist+1));
+                q.add(new Pair(row , col+1 , dist+1));
+                q.add(new Pair(row , col-1 , dist+1));
             }
         }
-        // If no path is found from source to destination, return -1
         return -1;
     }
-}
-
-
+};
