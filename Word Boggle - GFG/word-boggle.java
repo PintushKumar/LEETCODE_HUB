37b80fe0537622a3ca6bcc6fcbd9d6a8b09f1ed6
Pintush  Kumar <pintushmitcse@gmail.com>
2023-09-28 23:47:32 +0530

    Added solution - LeetHub

--- Word Boggle - GFG/word-boggle.java
+++ Word Boggle - GFG/word-boggle.java
@@ -55,6 +55,68 @@ class GFG
 
 //User function Template for Java
 
+// class Solution {
+    // public boolean dfs(char board[][], int row, int col, int numRows, int numCols, String word, int index) {
+    //     // Base case: If the entire word has been found, return true
+    //     if (index == word.length()) {
+    //         return true;
+    //     }
+    //     // Check for boundary conditions and character match
+    //     if (row < 0 || row >= numRows || col < 0 || col >= numCols || board[row][col] != word.charAt(index)) {
+    //         return false;
+    //     }
+    //     char currentChar = board[row][col];
+    //     // Mark the current cell as visited
+    //     board[row][col] = '*';
+    //     // Explore in all 8 possible directions
+    //     boolean right = dfs(board, row, col + 1, numRows, numCols, word, index + 1);
+    //     boolean left = dfs(board, row, col - 1, numRows, numCols, word, index + 1);
+    //     boolean down = dfs(board, row + 1, col, numRows, numCols, word, index + 1);
+    //     boolean up = dfs(board, row - 1, col, numRows, numCols, word, index + 1);
+    //     boolean upright = dfs(board, row + 1, col + 1, numRows, numCols, word, index + 1);
+    //     boolean upleft = dfs(board, row - 1, col - 1, numRows, numCols, word, index + 1);
+    //     boolean downright = dfs(board, row + 1, col - 1, numRows, numCols, word, index + 1);
+    //     boolean downleft = dfs(board, row - 1, col + 1, numRows, numCols, word, index + 1);
+    //     // Restore the original character at this cell
+    //     board[row][col] = currentChar;
+    //     // Return true if the word is found in any of the 8 directions
+    //     return right || left || down || up || upright || upleft || downright || downleft;
+    // }
+
+//     public String[] wordBoggle(char board[][], String[] dictionary) {
+        // // Initialize the result list
+        // ArrayList<String> result = new ArrayList<>();
+        // int numRows = board.length;
+        // int numCols = board[0].length;
+        // HashSet<String> set = new HashSet<>();
+        // // Loop through each word in the dictionary
+        // for (int wordIndex = 0; wordIndex < dictionary.length; wordIndex++) {
+        //     String targetWord = dictionary[wordIndex];
+        //     // Loop through the entire board
+        //     for (int row = 0; row < numRows; row++) {
+        //         for (int col = 0; col < numCols; col++) {
+        //             // Check if the first character of the word matches the current cell
+        //             if (board[row][col] == targetWord.charAt(0)) {
+        //                 // If the word is found, add it to the result list
+        //                 if (dfs(board, row, col, numRows, numCols, targetWord, 0)) {
+        //                     if (!set.contains(targetWord)) {
+        //                         result.add(targetWord);
+        //                         set.add(targetWord);
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // // Convert the result to a string array
+        // String finalResult[] = new String[result.size()];
+        // for (int i = 0; i < result.size(); i++) {
+        //     finalResult[i] = result.get(i);
+        // }
+        // return finalResult;
+//     }
+// }
+
 class Solution
 {
     public boolean dfs(char board[][] , int i , int j , int n , int m , String word , int indx){
@@ -104,4 +166,6 @@ class Solution
         }
         return finalRes;
     }
-}
+} 
+
+
