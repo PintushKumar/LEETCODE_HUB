4201a3e31c1a9a0f5322d461495ffaca65fcde9d
Pintush  Kumar <pintushmitcse@gmail.com>
2023-10-03 20:09:11 +0530

    Time: 21 ms (41.12%), Space: 54.9 MB (41.47%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,44 +1,89 @@
+// *********************** BFS **************************************
 class Solution {
-        public boolean  dfs(int[][] graphs , int currNode , boolean visited[]  , boolean dfsVisited[]){
-        visited[currNode]=true;
-        dfsVisited[currNode]=true;
-        for( int nbr : graphs[currNode]){
-            if(!visited[nbr]){
-                if( dfs(graphs , nbr , visited , dfsVisited )){
-                    return true;
-                }
-            }else if(dfsVisited[nbr]== true){
-                return true;
-            }
-        }
-        dfsVisited[currNode]=false;
-        return false;
-    }
-    public List<Integer> eventualSafeNodes(int[][] graphs) {
-        int V = graphs.length;
-        // List<List<Integer>> adj = new ArrayList<>();
-        // for(int i=0;i<V;i++){
-        //     adj.add(new ArrayList<>());
-        // }
-        // for(int graph[] :graphs){
-        //     int u = graph[0];
-        //     int v = graph[1];
-        //     adj.get(u).add(v);
-        // }
-        boolean visited[] = new boolean [V];
-        boolean dfsVisited[] = new boolean [V];
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int V = graph.length;
+        List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<V;i++){
-            if(!visited[i]){
-                dfs(graphs , i , visited , dfsVisited);
+            adj.add(new ArrayList<>());
+        }
+        for(int u=0;u<V;u++){
+            for(int v : graph[u]){
+                adj.get(v).add(u);
             }
         }
-        List<Integer> res = new ArrayList<>();
+        
+        int inDegree[] = new int [V];
+        for(int i=0;i<V;i++){
+            for(int v : adj.get(i)){
+                inDegree[v]++;
+            }
+        }
+        
+        Queue<Integer> q = new ArrayDeque<>();
         
         for(int i=0;i<V;i++){
-            if(dfsVisited[i]==false){
-                res.add(i);
+            if(inDegree[i]==0){
+                q.add(i);
             }
         }
-        return res;
+        
+        boolean safe [] = new boolean[V];
+        
+        while(q.size()>0){
+            int currNode = q.remove();
+            safe[currNode] = true;
+            for(int v : adj.get(currNode)){
+                inDegree[v]--;
+                if(inDegree[v]==0){
+                    q.add(v);
+                }
+            }
+        }
+        
+        List<Integer> resSafe = new ArrayList<>();
+        
+        for(int i=0;i<V;i++){
+            if(safe[i]== true)
+                resSafe.add(i);
+        }
+        return resSafe;
     }
 }
+
+// *********************** DFS ************************************
+
+// class Solution {
+//         public boolean  dfs(int[][] graphs , int currNode , boolean visited[]  , boolean dfsVisited[]){
+//         visited[currNode]=true;
+//         dfsVisited[currNode]=true;
+//         for( int nbr : graphs[currNode]){
+//             if(!visited[nbr]){
+//                 if( dfs(graphs , nbr , visited , dfsVisited )){
+//                     return true;
+//                 }
+//             }else if(dfsVisited[nbr]== true){
+//                 return true;
+//             }
+//         }
+//         dfsVisited[currNode]=false;
+//         return false;
+//     }
+//     public List<Integer> eventualSafeNodes(int[][] graphs) {
+//         int V = graphs.length;
+//         boolean visited[] = new boolean [V];
+//         boolean dfsVisited[] = new boolean [V];
+//         for(int i=0;i<V;i++){
+//             if(!visited[i]){
+//                 dfs(graphs , i , visited , dfsVisited);
+//             }
+//         }
+//         List<Integer> res = new ArrayList<>();
+        
+//         for(int i=0;i<V;i++){
+//             if(dfsVisited[i]==false){
+//                 res.add(i);
+//             }
+//         }
+//         return res;
+//     }
+// }
