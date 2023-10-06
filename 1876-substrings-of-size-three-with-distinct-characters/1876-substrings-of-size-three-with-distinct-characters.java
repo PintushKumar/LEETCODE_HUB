d3b035e6da8ac0b204d3414331b623a444ded965
Pintush  Kumar <pintushmitcse@gmail.com>
2023-10-07 00:19:23 +0530

    Time: 3 ms (31.34%), Space: 40.3 MB (89.33%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,29 +1,34 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        int i=0 , j=0;
-        int count =0;
-        HashMap<Character , Integer> map = new HashMap<>();
-        while(i<s.length()){
+        int i = 0;  // Initialize the 'i' pointer to the beginning of the string.
+        int j = 0;  // Initialize the 'j' pointer to the beginning of the string.
+        int count = 0;  // Initialize the count to keep track of good substrings.
+        HashMap<Character, Integer> map = new HashMap<>();  // Create a HashMap to store character counts.
+
+        while (i < s.length()) {
+            // Add the character at the current position to the map and update its count.
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
 
-            if((i-j+1)==3 ){
-                if(map.size()==3){
-                    count++;
+            if ((i - j + 1) == 3) {  // Check if the length of the current substring is 3.
+                if (map.size() == 3) {  // Check if there are 3 distinct characters in the substring.
+                    count++;  // Increment the count for a good substring.
+
                 }
-                if(map.containsKey(s.charAt(j))){
+                // Remove the character at the beginning of the substring.
+                if (map.containsKey(s.charAt(j))) {
                     int freq = map.get(s.charAt(j));
-                    if(freq ==1){
-                        map.remove(s.charAt(j));
-                    }else{
-                        map.put(s.charAt(j) , map.getOrDefault(s.charAt(j) , 0)-1);
+                    if (freq == 1) {
+                        map.remove(s.charAt(j));  // If only one occurrence, remove from the map.
+                    } else {
+                        map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) - 1);  // Decrement the count.
                     }
                 }
-                j++;
+                j++;  // Move the 'j' pointer to the next character in the substring.
             }
-           
-            
-            i++;
+
+            i++;  // Move the 'i' pointer to the next character in the string.
         }
-        return count;
+
+        return count;  // Return the total count of good substrings.
     }
 }
