291afd5c35b2dc8057e2675a45d7460834d675d3
Pintush  Kumar <pintushmitcse@gmail.com>
2023-10-23 08:03:30 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (81.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -30,32 +30,32 @@
 //     }
 // }
 
-class Solution {
-    public int fib(int n) {
-        if(n==0 || n==1 ){
-            return n;
-        }
-        int dp[] = new int [31];
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-    }
-}
-
 // class Solution {
 //     public int fib(int n) {
 //         if(n==0 || n==1 ){
 //             return n;
 //         }
-//         int first = 0 , second = 1 , third = 0;
+//         int dp[] = new int [31];
+//         dp[0]=0;
+//         dp[1]=1;
 //         for(int i=2;i<=n;i++){
-//             third = first + second;
-//             first = second;
-//             second = third;
+//             dp[i] = dp[i-1]+dp[i-2];
 //         }
-//         return third;
+//         return dp[n];
 //     }
 // }
+
+class Solution {
+    public int fib(int n) {
+        if(n==0 || n==1 ){
+            return n;
+        }
+        int first = 0 , second = 1 , third = 0;
+        for(int i=2;i<=n;i++){
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return third;
+    }
+}
