3755c1f44180671f39ea18633dc08f921efefc30
Pintush  Kumar <pintushmitcse@gmail.com>
2023-10-23 08:02:29 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (69.61%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,61 @@
 // Simple solution Using Recursion 
 
+// class Solution {
+//     public int fib(int n) {
+//         if(n==0 || n==1){
+//             return n;
+//         }
+//         return fib(n-1)+fib(n-2);
+//     }
+// }
+
+/// DP Approach 
+
 class Solution {
-    public int fib(int n) {
-        if(n==0 || n==1){
+    public int helper(int n , int dp[]){
+        if(n==0 || n==1 ){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        dp[n] = helper(n-1 , dp) + helper(n-2 , dp);
+        return dp[n];
+    }
+    public int fib(int n) {
+        int dp[] = new int [31];
+        Arrays.fill(dp , -1);
+        return helper(n , dp);
     }
 }
+
+// class Solution {
+//     public int fib(int n) {
+//         if(n==0 || n==1 ){
+//             return n;
+//         }
+//         int dp[] = new int [31];
+//         dp[0]=0;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             dp[i] = dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+// class Solution {
+//     public int fib(int n) {
+//         if(n==0 || n==1 ){
+//             return n;
+//         }
+//         int first = 0 , second = 1 , third = 0;
+//         for(int i=2;i<=n;i++){
+//             third = first + second;
+//             first = second;
+//             second = third;
+//         }
+//         return third;
+//     }
+// }
