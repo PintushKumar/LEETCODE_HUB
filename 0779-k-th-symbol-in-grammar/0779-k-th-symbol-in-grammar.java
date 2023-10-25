78e377ab3c90ce5517ed518cb6f0127e04a50e56
Pintush  Kumar <pintushmitcse@gmail.com>
2023-10-25 13:27:28 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (56.73%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -1,13 +1,22 @@
 class Solution {
     public int kthGrammar(int n, int k) {
-        if(n==1 && k==1){
+        // Base case: When n is 1 and k is 1, return 0.
+        if (n == 1 && k == 1) {
             return 0;
         }
-        int length = (int) Math.pow(2 , n-1);
-        int mid = (length/2);
-        if(k<=mid){
-            return kthGrammar(n-1 , k);
+        
+        // Calculate the length of the current row using 2^(n-1).
+        int length = (int) Math.pow(2, n - 1);
+        
+        // Calculate the mid-point of the row.
+        int mid = (length / 2);
+        
+        // If k is less than or equal to the mid-point, recursively call kthGrammar on the previous row with the same k.
+        if (k <= mid) {
+            return kthGrammar(n - 1, k);
         }
-        return 1- kthGrammar(n-1 , k-mid);
+        
+        // If k is greater than the mid-point, recursively call kthGrammar on the previous row with k adjusted by subtracting mid.
+        return 1 - kthGrammar(n - 1, k - mid);
     }
 }
