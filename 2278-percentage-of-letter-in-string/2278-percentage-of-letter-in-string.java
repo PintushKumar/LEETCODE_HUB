88415866b15a7c7041b5627baf8c241b6b94f91e
Pintush  Kumar <pintushmitcse@gmail.com>
2023-11-07 22:17:08 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (38.44%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.java
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.java
@@ -1,16 +1,16 @@
 class Solution {
     public int percentageLetter(String s, char letter) {
-        HashMap<Character , Integer> map = new HashMap<>();
+        int cnt=0;
         for(int i=0;i<s.length();i++){
-            map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0)+1);
+           if(s.charAt(i)==letter){
+               cnt++;
+           }
         }
         
-        if(!map.containsKey(letter)){
+        if(cnt==0){
             return 0;
         }
-        
-        int freq = map.get(letter);
-        
-        return (freq*100)/s.length();
+   
+        return (cnt*100)/s.length();
     }
 }
