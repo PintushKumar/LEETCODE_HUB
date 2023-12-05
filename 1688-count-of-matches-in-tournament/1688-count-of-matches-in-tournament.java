cfa9b7f8935d021929155c80c3ed05896572dd15
Pintush  Kumar <pintushmitcse@gmail.com>
2023-12-05 06:51:18 +0530

    Time: 0 ms (100.00%), Space: 40 MB (5.39%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,16 +1,16 @@
 class Solution {
     public int numberOfMatches(int n) {
-        int result =0;
-        while(n>1){
-            if(n%2==0){
-                result+= n/2;
-                n/=2;
-            }else{
-                result+=(n-1)/2;
-                n= ((n-1)/2)+1;
-            }
-        }
-        return result;
+        // int result =0;
+        // while(n>1){
+        //     if(n%2==0){
+        //         result+= n/2;
+        //         n/=2;
+        //     }else{
+        //         result+=(n-1)/2;
+        //         n= ((n-1)/2)+1;
+        //     }
+        // }
+        // return result;
         
         // int cnt =0;
         // while(n>1){
@@ -18,6 +18,8 @@ class Solution {
         //     n=(n+1)/2;
         // }
         // return cnt;
+        
+        return n-1;
 
     }
 }
